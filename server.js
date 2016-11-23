76821f4ed3c2d6468fea25a5263313d350c451e0
Thomas Gallin <thomas.gallin@gmail.com>
2016-11-23 21:20:44 +0000

    updating server.js to retrieve information from request header

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.end('hello world');
+    var ipaddress = req.get('x-forwarded-for');
+    var useragent = req.get('user-agent');
+    var software = useragent.substring(useragent.indexOf('(') + 1, useragent.indexOf(')'));
+    var acceptlanguage = req.get('accept-language');
+    var language = acceptlanguage.substring(0, acceptlanguage.indexOf(','));
+    // we want to retrieve from the header
+    // {"ipaddress":"90.107.15.61","language":"en-US","software":"Windows NT 10.0; WOW64"}
+    var result = {
+        ipaddress: ipaddress,
+        language: language,
+        software: software
+    };
+    res.json(result);
 });
 
 var port = process.env.PORT || 8080;
